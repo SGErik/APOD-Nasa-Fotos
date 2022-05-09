18b8543e12647e0d69beda2324e5f4b6ae08d706
Erik Souza <erik.souzaxbr@gmail.com>
2022-05-09 05:53:25 -0300

    Deixe o site completamente funcional

---
+++ index.js
@@ -0,0 +1,30 @@
+var date = $("#inputData").val();
+var recebeVideo = $("#receiveVideo");
+var recebeImagem = $("#receiveImg");
+
+
+
+
+$('#btnDate').click(function () {
+    var date = $("#inputData").val();
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=gcZG2YgEeGIBCoRRxFtwCbOOhqepwpdLNzBLSaUo&date=${date}`,
+        type: "GET",
+
+        success: function (info) {
+            console.log(info)
+            $("h2").text(info.title);
+            if (info.media_type == "image" || info.media_type == "gif") {
+                recebeImagem.attr("src", info.url).css("display", "flex")
+                recebeVideo.css("display", "none")
+            }else if(info.media_type == "video"){
+                recebeVideo.attr("src", info.url).css("display", "flex")
+                recebeImagem.css("display", "flex")
+            }
+            $("p").text(info.explanation)
+            }
+
+    })
+
+})
+
