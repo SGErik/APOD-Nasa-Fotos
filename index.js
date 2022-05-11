9316642c72ea2bb3390b67ec2da010496c27895e
SGErik <erik.souzaxbr@gmail.com>
2022-05-11 15:11:44 -0300

    Troquei todas as "var" por "let"

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var date = $("#inputData").val();
-var recebeVideo = $("#receiveVideo");
-var recebeImagem = $("#receiveImg");
-var imgAndVideo = $("#receiveImgOrVideo")
-var dateInput = $('#inputDate')
+let date = $("#inputData").val();
+let recebeVideo = $("#receiveVideo");
+let recebeImagem = $("#receiveImg");
+let imgAndVideo = $("#receiveImgOrVideo")
+let dateInput = $('#inputDate')
 
 
 
 
 
 $('#btnDate').click(function () {
-    var date = $("#inputData").val();
+    let date = $("#inputData").val();
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=gcZG2YgEeGIBCoRRxFtwCbOOhqepwpdLNzBLSaUo&date=${date}`,
         type: "GET",
@@ -19,14 +19,14 @@ $('#btnDate').click(function () {
             if (info.media_type == "image" || info.media_type == "gif") {
                 recebeImagem.attr("src", info.url).css("display", "flex")
                 recebeVideo.css("display", "none")
-            }else if(info.media_type == "video"){
+            } else if (info.media_type == "video") {
                 recebeVideo.attr("src", info.url).css("display", "flex")
                 recebeImagem.css("display", "none")
             }
             $("p").text(info.explanation)
             imgAndVideo.css("display", "flex")
             dateInput.css("margin-top", "30px")
-            }
+        }
 
     })
 
